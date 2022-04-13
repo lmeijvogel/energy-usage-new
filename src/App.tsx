80e2b4cde7dba869215f73e76a6e4dc9424d6280
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-04-13 13:25:45 +0200

    Render simple graph from backend API

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+import Chart from "react-apexcharts";
+
+export const App = () => {
+  const defaultState = {
+    options: {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }
+    ]
+  };
+
+  const [state, setState] = useState(defaultState);
+
+  const setNewValues = (json: any) => {
+    const labels = json.map((value: any) => value[0]);
+    const values = json.map((value: any) => value[1]);
+
+    setState({
+      ...state,
+      options: {
+        ...state.options,
+        xaxis: {
+          categories: labels
+        }
+      },
+      series: [
+        {
+          name: "gas",
+          data: values
+        }
+      ]
+    });
+  }
+
+  useEffect(() => {
+    fetch("/api/gas").then(response => response.json())
+      .then(json => setNewValues(json));
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <div className="row">
+        <div className="mixed-chart">
+          <Chart
+          options={state.options}
+    series={state.series}
+    type="bar"
+          width="500"
+        />
+          </div>
+        </div>
+      </div>
   );
 }
 
